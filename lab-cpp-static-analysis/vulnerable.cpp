149c4da04c3e717cae1cd93eabb4315280025a92
Keith C. Canty <keith.curtis.canty@gmail.com>
2025-11-22 23:46:26 -0700

    Update vulnerable.cpp

--- lab-cpp-static-analysis/vulnerable.cpp
+++ lab-cpp-static-analysis/vulnerable.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void insecure_copy(char *input) {
+    char buffer[10];
+    strcpy(buffer, input); // insecure: no bounds checking
+    cout << "Copied: " << buffer << endl;
+}
+
+int main(int argc, char *argv[]) {
+    char userInput[256];
+    cout << "Enter input: ";
+    cin >> userInput; // insecure: no input size validation
+    insecure_copy(userInput);
+    return 0;
+}
 
